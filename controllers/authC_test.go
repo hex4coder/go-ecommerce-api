278b1a2350c14ddd151dd0ed0853d8994bce75fe
hex4coder <the.programmer.luyo@gmail.com>
2024-12-22 12:16:01 +0800

    membuat testing untuk check bcrypt go dan laravel

--- controllers/authC_test.go
+++ controllers/authC_test.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPasswordHash(t *testing.T) {
 	hashedPassword := "$2y$12$lksGE6.lHRcG5BW3MNBu/eO7jRJxu8QcU6wRdGUkewvnEI9K28fw2"
@@ -11,4 +14,16 @@ func TestPasswordHash(t *testing.T) {
 	if ok := api.CheckPassword(password, hashedPassword); !ok {
 		t.Fatal("password tidak valid")
 	}
+}
+
+func TestHashingPassword(t *testing.T) {
+	password := "12345678"
+	api := NewAuthAPI(nil)
+
+	hashed, err := api.HashPassword(password)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	fmt.Printf("Hasil : %s", hashed)
 }
