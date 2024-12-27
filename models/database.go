ed8be68f9078d312726d1d9dc782c716f479d4cc
hex4coder <the.programmer.luyo@gmail.com>
2024-12-27 10:56:38 +0800

    Penyesuaian mysql user pada server

--- models/database.go
+++ models/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	database, err := gorm.Open(mysql.Open("root@tcp(127.0.0.1:3306)/ecommercebalanipa?parseTime=true"))
+	database, err := gorm.Open(mysql.Open("ecom:ecom123;@tcp(127.0.0.1:3306)/ecommercebalanipa?parseTime=true"))
 	if err != nil {
 		return nil, fmt.Errorf("database connection failed")
 	}
